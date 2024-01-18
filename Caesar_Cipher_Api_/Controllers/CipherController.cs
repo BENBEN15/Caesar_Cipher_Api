c01902b4f77da29321e7b28f050c6f806e841d9f
BENBEN15 <benoit.serre@ocsalis.fr>
2024-01-18 14:26:38 +0100

    Fix (cipher controller) - fixed http verb - fixed validation - removed logger - added cypher service

--- Caesar_Cipher_Api_/Controllers/CipherController.cs
+++ Caesar_Cipher_Api_/Controllers/CipherController.cs
@@ -8,37 +8,27 @@ namespace Caesar_Cipher_Api.Controllers
     [Route("[controller]")]
     public class CipherController : ControllerBase
     {
-        private readonly ILogger<CipherController> _logger;
         private readonly ICipherService _cipherService;
 
-        public CipherController(ILogger<CipherController> logger, ICipherService cipherService)
+        public CipherController(ICipherService cipherService)
         {
-            _logger = logger;
             _cipherService = cipherService;
         }
 
-        [HttpGet, Route("/encrypt")]
+        [HttpPost, Route("/encrypt")]
         public IActionResult Encrypt(CipherRequest request)
         {
-            if (request == null) return BadRequest(new BaseResponse(false, 400, null, "missing request body"));
-
             if (request.Offset < 0) return BadRequest(new BaseResponse(false, 400, null, "offset needs to be a positive number"));
-
-            if (request.Message == null) return BadRequest(new BaseResponse(false, 400, null, "request is missing the message"));
 
             string encrypted = _cipherService.Encrypt(request.Message, request.Offset);
 
             return Ok(new CipherResponse(true, 200, request.Offset, encrypted));
         }
 
-        [HttpGet, Route("/decrypt")]
+        [HttpPost, Route("/decrypt")]
         public IActionResult Decrypt(CipherRequest request)
         {
-            if (request == null) return BadRequest(new BaseResponse(false, 400, null, "missing request body"));
-
             if (request.Offset < 0) return BadRequest(new BaseResponse(false, 400, null, "offset needs to be a positive number"));
-
-            if (request.Message == null) return BadRequest(new BaseResponse(false, 400, null, "request is missing the message"));
 
             string decrypted = _cipherService.Decrypt(request.Message, request.Offset);
 
