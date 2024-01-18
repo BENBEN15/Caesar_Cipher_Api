9a2439b6e76f556903ce17a6589d5cfc2847616d
BENBEN15 <benoit.serre@ocsalis.fr>
2024-01-18 11:30:35 +0100

    Feature (Controllers) - added base controller Cipher - removed default weather controller

---
+++ Caesar_Cipher_Api_/Controllers/CipherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Caesar_Cipher_Api_.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CipherController : ControllerBase
+    {
+        private readonly ILogger<CipherController> _logger;
+
+        public CipherController(ILogger<CipherController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
